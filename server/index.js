d17a27c8e42eb93e7d6581a045a5359c862c8b7a
Divesh <diveshp904@gmail.com>
2024-06-18 13:49:24 +0530

    Added

---
+++ server/index.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const authRoutes = require("./Routes/auth");
+const noteRoutes = require("./Routes/notes");
+dotenv.config({ path: ".env" });
+const app = express();
+// const allowedOrigins = ['http://localhost:5174'];
+const PORT = 6969;
+
+
+
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+
+
+}));
+app.use(bodyParser.json());
+app.use(express.json());
+
+
+try {
+    mongoose.connect(process.env.MONGO_URL);
+    console.log("Connection Successfull");
+} catch (error) {
+    console.log(error);
+}
+
+app.get("/", (req, res) => {
+    res.send("Server Is Running");
+});
+
+
+app.use("/auth", authRoutes);
+app.use("/notes", noteRoutes);
+app.use("/files", express.static("files"));
+
+app.listen(PORT, () => {
+    console.log(`Server Running on Port ${PORT}`);
+})
